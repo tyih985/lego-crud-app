3413568565059d466d78cc92d947385bbb683102
tyih985 <tyler.yih985@gmail.com>
2025-09-07 11:19:06 -0700

    Initial commit.

---
+++ src/main/java/com/lego/lego_sets/LegoSetsApplication.java
@@ -0,0 +1,13 @@
+package com.lego.lego_sets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LegoSetsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LegoSetsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lego/lego_sets/LegoSetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lego.lego_sets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LegoSetsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
